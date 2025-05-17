a3e0a0fe158a39119596ff5fb765c37b0c4c3a75
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-05-17 15:46:19 +0700

    commit 14

--- LatihanPertemuan9B.cpp
+++ LatihanPertemuan9B.cpp
@@ -2,3 +2,24 @@
 #include <string>
 using namespace std;
 
+// Forward declaration
+class Petugas;
+class Admin;
+
+class Buku {
+private:
+    string judul, penulis;
+    bool dipinjam;
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    void info() {
+        cout << "Judul: " << judul << " | Penulis: " << penulis
+             << " | Status: " << (dipinjam ? "Dipinjam" : "Tersedia") << endl;
+    }
+
+    friend class Petugas;
+    friend class Admin;
+};
+
